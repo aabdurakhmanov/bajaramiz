b5ce5b0c23569c30c7578c65b44804380d7e1731
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2021-12-25 23:32:16 +0500

    change some

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
+    objects = None
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
--- base/views.py
+++ base/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render, redirect
-from django.views.generic.list import ListView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.db import transaction
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
-
-# Imports for Reordering Feature
-from django.views import View
-from django.shortcuts import redirect
-from django.db import transaction
-
-from .models import Task
 from .forms import PositionForm
+from .models import Task
 
 
 class CustomLoginView(LoginView):
